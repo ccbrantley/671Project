d6034547b118da4655e9ed4aa7318d416bd7b46a
ccbrantley <c_brantl@uncg.edu>
2021-10-31 09:53:28 -0400

    Moving indentation back one tab.

--- views/products.php
+++ views/products.php
@@ -1,4 +1,4 @@
 <?php
-	include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
-	include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
 ?>
