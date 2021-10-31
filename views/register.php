82d131c4c7fa09be28594e65e3e9704feb97ec38
ccbrantley <c_brantl@uncg.edu>
2021-10-31 09:56:08 -0400

    Adding class names to style status message based on success/failure.

--- views/register.php
+++ views/register.php
@@ -1,97 +1,91 @@
 <?php
-	include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
-	include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
-	$registrationResponse = "";
-	if (isset($_POST['registrationSubmit'])) {
-		include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
-		$userNameValid = uniqueUserName($_POST['userName']);
-		if ($userNameValid !== NULL) {
-			if ($userNameValid == True) {
-				if (registerUser(array(
-				$_POST['userName'],
-				$_POST['password'],
-				$_POST['firstName'],
-				$_POST['lastName'],
-				$_POST['streetAddress'],
-				$_POST['cityAddress'],
-				$_POST['zipAddress'],
-				$_POST['stateAddress'],
-				$_POST['countryAddress'],
-				$_POST['creditCard'])) !=
-				NULL) {
-					$registrationResponse = "Registration was successful.";
-				}
-				else {
-					$registrationResponse = "Registration was not successful.";
-				}
+include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
+$registrationResponse = "";
+if (isset($_POST['registrationSubmit'])) {
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
+	$userNameValid = uniqueUserName($_POST['userName']);
+	if ($userNameValid !== NULL) {
+		if ($userNameValid == True) {
+			if (registerUser(array(
+			$_POST['userName'],
+			$_POST['password'],
+			$_POST['firstName'],
+			$_POST['lastName'],
+			$_POST['streetAddress'],
+			$_POST['cityAddress'],
+			$_POST['zipAddress'],
+			$_POST['stateAddress'],
+			$_POST['countryAddress'],
+			$_POST['creditCard'])) !=
+			NULL) {
+				$registrationResponse = "<p class = 'successText'>";
+				$registrationResponse .= "Registration was successful!</p>";
 			}
 			else {
-				$registrationResponse = "Username is already taken, please choose a different one.";
+				$registrationResponse = "<p class = 'failedText'>";
+				$registrationResponse .= "Registration was not successful.</p>";
 			}
 		}
 		else {
-			$registrationResponse = "Registration was not successful.";
+			$registrationResponse = "<p class = 'failedText'>";
+			$registrationResponse .= "Username is already taken, please choose a different one.</p>";
 		}
 	}
-	echo <<<EOD
-		<h1>Registration</h1>
-		<form class = "registerForm" action = "" method = "post">
-			<div>
-				<p>
+	else {
+		$registrationResponse = "<p class = 'failedText'>";
+		$registrationResponse = "Registration was not successful.</p>";
+	}
+}
+echo <<<EOD
+	<h1>Registration</h1>
+	<form class = "userForm" action = "" method = "post">
+		<div>
+			<p>
 				<label for = "userName">User Name:</label>
 				<input type = "text" id = "userName" name = "userName" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "password">Password:</label>
 				<input type = "password" id = "password" name = "password" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "firstName">First Name:</label>
 				<input type = "text" id = "firstName" name = "firstName" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "lastName">Last Name:</label>
 				<input type = "text" id = "lastName" name = "lastName" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "streetAddress">Street:</label>
 				<input type = "text" id = "streetAddress" name = "streetAddress" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "cityAddress">City:</label>
 				<input type = "text" id = "cityAddress" name = "cityAddress" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "zipAddress">Zip:</label>
 				<input type = "text" id = "zipAddress" name = "zipAddress" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "stateAddress">State:</label>
 				<input type = "text" id = "stateAddress" name = "stateAddress" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "countryAddress">Country:</label>
 				<input type = "text" id = "countryAddress" name = "countryAddress" required>
-				</p>
-
-				<p>
+			</p>
+			<p>
 				<label for = "creditCard">Credit Card:</label>
 				<input type = "text" id = "creditCard" name = "creditCard" required>
-				</p>
-			</div>
-
-			<div>
-				<input type = "submit" name = "registrationSubmit" value = "Register">
-			</div>
-		</form>
-		<p class = "registrationResponse">$registrationResponse</p>
-	EOD;
+			</p>
+		</div>
+		<div>
+			<input type = "submit" name = "registrationSubmit" value = "Register">
+		</div>
+	</form>
+	$registrationResponse
+EOD;
 ?>
