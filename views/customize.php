54cbe2e3697ca271d4f8efe2f603e1eea67747db
ccbrantley <c_brantl@uncg.edu>
2021-11-02 20:48:24 -0400

    Temporarily set up to display base id for system to customize.

--- views/customize.php
+++ views/customize.php
@@ -1,4 +1,11 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
-include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
+echo <<<EOD
+<div class = 'divPage'>
+EOD;
+echo "<h1>Customizing {$_POST['base_id']}</h1>";
+echo <<<EOD
+</div>
+EOD;
+include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';                                                                                                                    
 ?>
