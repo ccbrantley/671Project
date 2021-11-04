6279ce4dc836c87a56e5bb899b09dd4d25af83f7
ccbrantley <c_brantl@uncg.edu>
2021-11-04 13:50:07 -0400

    Joined similar code for creating product tables into single function that takes in arguments.

--- views/products.php
+++ views/products.php
@@ -1,5 +1,5 @@
 <?php
-function baseProductTableDisplay () {
+function productTableDisplay ($_arguments) {
 	include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
 	$actionPage = '/671Project/views/customize.php';
 	$table .= "<form class = 'formTable' action = '$actionPage' method = 'post'>";
@@ -9,11 +9,12 @@ function baseProductTableDisplay () {
 	$table .= '<div class = "divCell">Storage</div>';
 	$table .= '<div class = "divCell">OS</div>';
 	$table .= '<div class = "divCell">Specifications</div>';
+	$table .= '<div class = "divCell">Price</div>';
 	$table .= '<div class = "divCell">';
 	$table .= "<input type = 'submit' name = 'customize' value = 'Customize'>";
 	$table .= '</div>';
 	$table .= "</div>";
-	foreach (getBaseProducts() as $row) {
+	foreach ($_arguments as $row) {
 		$table .= '<div class = "divRow">';
 		$table .= "<div class = 'divCell'>{$row['processor_name']}</div>";
 		$table .= "<div class = 'divCell'>{$row['memory_size']} gb </div>";
@@ -27,6 +28,7 @@ function baseProductTableDisplay () {
 		$table .= "<li>" . $row['size'] . " inches</li>";
 		$table .= "<li>{$row['type']}</li>";
 		$table .= "</ul></div>";
+		$table .= "<div class = 'divCell'>\${$row['price']}</div>";
 		$table .= "<div class = 'divCell'>";
 		$table .= "<input type = 'radio' name = 'base_id' value = '{$row['base_id']}'>";
 		$table .= "</div>";
@@ -34,6 +36,14 @@ function baseProductTableDisplay () {
 	}
 	$table .= "</form>";
 	return $table;
+}
+function baseProductTableDisplay () {
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
+	return productTableDisplay(getBaseProducts());
+}
+function searchBaseProductTableDisplay () {
+	include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
+	return productTableDisplay(searchBaseProducts());
 }
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
 echo <<<EOD
@@ -43,18 +53,18 @@ echo <<<EOD
 			<div class = "searchBarDiv">
 				<p>
 					<label for "maxPrice">Max Price:</label>
-					<input id = "maxPrice" name = "maxPrice" type = "text">
+					<input id = "maxPrice" name = "maxPrice" type = "text" pattern = "[0-9]*">
 				</p>
 				<p>
 					<label for "maxWeight">Max Weight (grams):</label>
-					<input id = "maxWeight" name = "maxWeight" type = "text">
+					<input id = "maxWeight" name = "maxWeight" type = "text" pattern = "[0-9]*">
 				</p>
 				<p>
 					<label for "size">Size (in):</label>
-					<input id = "size" name = "size" type = "text">
+					<input id = "size" name = "size" type = "number" min = "6">
 				</p>
 					<label for "laptop">Laptops:</label>
-					<input id = "laptop" name = "laptop" type = "checkbox">
+					<input id = "laptops" name = "laptops" type = "checkbox">
 				<p>
 				</p>
 					<label for "tablet">Tablets:</label>
@@ -67,13 +77,18 @@ echo <<<EOD
 				</p>
 			</div>
 			<div>
-				<input type = "submit" value = "Search">
+				<input type = "submit" name = 'search' value = "Search">
 			</div>
 		</form>
 	</div>
 	<div class = "productDisplay">
 EOD;
-echo baseProductTableDisplay();
+if (!isset($_POST['search'])) {
+	echo baseProductTableDisplay();
+}
+else {
+	echo searchBaseProductTableDisplay();
+}
 echo <<<EOD
 	</div>
 </div>
