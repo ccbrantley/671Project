93b679341cd383b9e244c87356a2515cb3297697
ccbrantley <c_brantl@uncg.edu>
2021-11-06 06:00:10 -0400

    Adding temporary code to display the product to be purchased.

--- views/purchase.php
+++ views/purchase.php
@@ -1,4 +1,9 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
+echo "<h1>Purchase</h1>";
+echo "Chosen base id: " . $_SESSION['chosen_base_id'] . "<br>";
+echo "Chosen memory id: " . $_SESSION['chosen_memory_id'] . "<br>";
+echo "Chosen storage id: " . $_SESSION['chosen_storage_id'] . "<br>";
+echo "Chosen os name: " . $_SESSION['chosen_os_name'] . "<br>";
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
 ?>
