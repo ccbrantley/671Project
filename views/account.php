aaf8aff815b23c882b672ace18dd4ffa75100f51
ccbrantley <c_brantl@uncg.edu>
2021-11-08 08:01:37 -0500

    Finishing wishlist and posting the wishlist_id temporarily, id to be used to finalize purchase.

--- views/account.php
+++ views/account.php
@@ -1,7 +1,10 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
+if (isset($_POST['wishListPurchase'])) {
+	echo 'Purchasing: ' . $_POST['wishlist_id'];
+}
 function displayPurchases () {
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
 	$purchases = getPurchases($_SESSION['user_id']);
 	if (!$purchases) {
 		return;
@@ -18,6 +21,7 @@ function displayPurchases () {
 		$storage = $storage->fetchAll()[0];
 		$os = preparedQuery("SELECT * FROM O_SYSTEM WHERE name = ?;", array($purchase['os_name']));
 		$os = $os->fetchAll()[0];
+		$base_system['weight'] = intval($base_system['weight']);
 		echo <<<EOD
 			<div class = 'divTableRow'>
 				<div class = 'divTableCell'>{$processor['name']}</div>
@@ -34,6 +38,40 @@ function displayPurchases () {
 				<div class = 'divTableCell'>{$purchase['price']}</div>
 				<div class = 'divTableCell'>{$purchase['date']}</div>
 				<div class = 'divTableCell'>{$purchase['status']}</div>
+			</div>
+		EOD;
+	}
+}
+function displayWishlist () {
+	$wishList = preparedQuery("SELECT * FROM WISHLIST WHERE user_id = ?;", array($_SESSION['user_id']));
+	foreach($wishList->fetchAll() as $item) {
+		$base_system = preparedQuery("SELECT * FROM BASE_SYSTEM WHERE base_id = ?;", array($item['base_id']));
+		$base_system = $base_system->fetchAll()[0];
+		$proc = preparedQuery("SELECT * FROM PROCESSOR WHERE processor_id = ?;", array($base_system['processor_id']));
+		$proc = $proc->fetchAll()[0];
+		$mem = preparedQuery("SELECT * FROM MEMORY WHERE memory_id = ?;", array($item['memory_id']));
+		$mem = $mem->fetchAll()[0];
+		$stor = preparedQuery("SELECT * FROM STORAGE WHERE storage_id = ?;", array($item['storage_id']));
+		$stor = $stor->fetchall()[0];
+		$os = preparedQuery("SELECT * FROM O_SYSTEM WHERE name = ?;", array($item['os_name']));
+		$os = $os->fetchAll()[0];
+		$base_system['weight'] = intval($base_system['weight']);
+		echo <<<EOD
+			<div class = 'divTableRow'>
+				<div class = 'divTableCell'>{$proc['name']}</div>
+				<div class = 'divTableCell'>{$mem['size']} gb</div>
+				<div class = 'divTableCell'>{$stor['size']} gb {$stor['type']}</div>
+				<div class = 'divTableCell'>{$os['name']}</div>
+				<div class = 'divTableCell'>
+					<ul>
+						<li>{$base_system['weight']} grams</li>
+						<li>{$base_system['size']} in</li>
+						<li>{$base_system['type']}</li>
+					</ul>
+				</div>
+				<div class = 'divTableCell'>
+					<input type = 'radio' name = 'wishlist_id' value = '{$item['wishlist_id']}'>
+				</div>
 			</div>
 		EOD;
 	}
@@ -56,6 +94,24 @@ EOD;
 displayPurchases();
 echo <<<EOD
 	</div>
+</div>
+<h1 class = "headerPadding">Wishlist</h1>
+<div class = "divTableHeight">
+	<form class = 'formTable' action = '' method = 'post'>
+		<div class = 'divTableHeader'>
+			<div class = 'divTableCell'>Processor</div>
+			<div class = 'divTableCell'>Memory</div>
+			<div class = 'divTableCell'>Storage</div>
+			<div class = 'divTableCell'>OS</div>
+			<div class = 'divTableCell'>Specifications</div>
+			<div class = 'divTableCell'>
+				<input type = "submit" name = 'wishListPurchase' value = "Purchase">
+			</div>
+		</div>
+EOD;
+displayWishList();
+echo <<<EOD
+	</form>
 </div>
 EOD;
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
