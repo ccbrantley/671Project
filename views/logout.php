b45e555cf0cbb650bcfcc7bb8c07e5636f653985
ccbrantley <c_brantl@uncg.edu>
2021-11-10 23:20:46 -0500

    Changing formatting, reducing lines for readability.

--- views/logout.php
+++ views/logout.php
@@ -4,9 +4,7 @@ session_unset();
 session_destroy();
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
 echo <<<EOD
-	<div class = "logoutDiv">
-		<p class = 'successText'>You have been successfully logged out.</p> 
-	</div>
+	<div class = "logoutDiv"><p class = 'successText'>You have been successfully logged out.</p></div>
 EOD;
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
 ?>
