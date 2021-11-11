139ec4b80967c1ec6532e1626b08684771c4f12d
ccbrantley <c_brantl@uncg.edu>
2021-11-10 23:22:42 -0500

    Removing redirect back to customization page from login condition being enforced. Not able to currently handle the condition that the user navigates away, user than will randomly be redirected at a time when they login.

--- views/login.php
+++ views/login.php
@@ -1,37 +1,28 @@
 <?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
 session_start();
-$loginResponse = '';
+$loginResponse = NULL;
 if (isset($_POST['loginSubmit'])) {
-	include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/tools/DBFunctions.php';
 	$user = loginUser(array($_POST['userName'], $_POST['password']));
 	if ($user !== NULL) {
 		$_SESSION['user_id'] = $user['user_id'];
 		$_SESSION['f_name'] = $user['f_name'];
-		if ($_SESSION['loginRedirect'] !== NULL) {
-			$redirect = $_SESSION['loginRedirect'];
-			$_SESSION['loginRedirect'] = NULL;
-			header($redirect);
-		}
-		$loginResponse = '<p class = "successText">';
-		$loginResponse .= 'Login was successful!</p>';
+		$loginResponse = '<p class = "successText">Login was successful!</p>';
 	}
 	else {
-		$loginResponse = '<p class = "failedText">';
 		$uniqueUserName = uniqueUserName($_POST['userName']);
 		if ($uniqueUserName === NULL) {
-			$loginResponse .= 'Login was not successful';
+			$loginResponse = '<p class = "failedText">Login was not successful.</p>';
 		}
 		else if ($uniqueUserName === False) {
-			$loginResponse .= 'Please check that your password is correct.';
+			$loginResponse = '<p class = "failedText">Please check that your password is correct.</p>';
 		}
 		else {
-			$loginResponse .= 'Please check that your username is correct.';
+			$loginResponse = '<p class = "failedText">Please check that your username is correct.</p>';
 		}
-		$loginResponse .= '</p>';
 	}
 }
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
-
 echo <<<EOD
 <h1>Login</h1>
 <form class = "standardForm" action = "/671Project/views/login.php" method = "post">
@@ -49,7 +40,7 @@ echo <<<EOD
 		<input type = "submit" name = "loginSubmit" value = "Login">
 	</div>
 </form>
-$loginResponse
 EOD;
+if ($loginResponse) { echo $loginResponse; }
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
 ?>
