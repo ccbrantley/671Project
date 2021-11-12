2f43699d4edaff67e198c96cb3acc955fd75d09e
ccbrantley <c_brantl@uncg.edu>
2021-11-12 01:10:52 -0500

    Setting up crud and stat pages with header/footer.

---
+++ admin/views/AdminUpdate.php
@@ -0,0 +1,4 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminHeader.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminFooter.php';
+?>
