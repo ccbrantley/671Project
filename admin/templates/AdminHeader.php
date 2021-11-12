7e5bf0551410d34ed71cd771bb4dc767960d71c5
ccbrantley <c_brantl@uncg.edu>
2021-11-12 01:07:33 -0500

    Page setup to only allow navigation to login, otherwise crud pages and logout is displayed.

---
+++ admin/templates/AdminHeader.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+echo <<<EOD
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title> 671 Project</title>
+		<link rel = "stylesheet" type = "text/css" href = "/671Project/style/admin.css">
+	</head>
+	<body>
+EOD;
+if (isset($_SESSION['admin_id'])) {
+	echo <<<EOD
+		<div class = "adminNavigation">
+			<a href = "/671Project/admin/views/AdminCreate.php">Create</a>
+			<a href = "/671Project/admin/views/AdminRead.php">Read</a>
+			<a href = "/671Project/admin/views/AdminUpdate.php">Update</a>
+			<a href = "/671Project/admin/views/AdminDelete.php">Delete</a>
+			<a href = "/671Project/admin/views/AdminStats.php">Statistics</a>
+			<a href = "/671Project/admin/views/AdminLogout.php">Logout</a>
+		</div>
+	EOD;
+}
+else {
+	echo <<<EOD
+		<div class = "adminNavigation">
+			<a href = "/671Project/admin/views/AdminLogin.php">Login</a>
+		</div>
+	EOD;
+}
+echo "<div class = 'lessBlankSpace'></div>";
+?>
