009084dc04adf22f62c096f6531c2c2aef643926
ccbrantley <c_brantl@uncg.edu>
2021-11-13 13:03:53 -0500

    Removing table arrays from read so they can be more easily accessed by all crud operation.

--- admin/views/AdminRead.php
+++ admin/views/AdminRead.php
@@ -1,152 +1,7 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminHeader.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/tools/AdminFunctions.php';
-$tables = [
-"customer" =>
-	[
-	"displayName" => "Customers",
-	"tableName" => "CUSTOMER",
-	"attributes" =>
-		[
-		'user_id',
-		'username',
-		'password',
-		'f_name',
-		'l_name',
-		'street',
-		'city',
-		'zip',
-		'state',
-		'country',
-		'credit_card',
-		],
-	],
-"wishList" =>
-	[
-	"displayName" => "Wish Lists",
-	"tableName" => "WISHLIST",
-	"attributes" => 
-		[
-		'wishlist_id',
-		'user_id',
-		'base_id',
-		'memory_id',
-		'storage_id',
-		'os_name',
-		],
-	],
-"purchase" =>
-	[
-	"displayName" => "Purchases",
-	"tableName" => "PURCHASE",
-	"attributes" =>
-		[
-		'purchase_id',
-		'user_id',
-		'base_id',
-		'memory_id',
-		'storage_id',
-		'os_name',
-		'price',
-		'status',
-		'date',
-		],
-	],
-"baseSystem" =>
-	[
-	"displayName" => "Base Systems",
-	"tableName" => "BASE_SYSTEM",
-	"attributes" =>
-		[
-		'base_id',
-		'processor_id',
-		'memory_id',
-		'storage_id',
-		'os_name',
-		'weight',
-		'size',
-		'price',
-		'type',
-		],
-	],
-"storage" =>
-	[
-	"displayName" => "Storage",
-	"tableName" => "STORAGE",
-	"attributes" =>
-		[
-		'storage_id',
-		'size',
-		'qty',
-		'price',
-		'type',
-		],
-	],
-"os" =>
-	[
-	"displayName" => "Operating Systems",
-	"tableName" => "O_SYSTEM",
-	"attributes" =>
-		[
-		'name',
-		'price',
-		],
-	],
-"memory" =>
-	[
-	"displayName" => "Memory",
-	"tableName" => "MEMORY",
-	"attributes" =>
-		[
-		'memory_id',
-		'size',
-		'qty',
-		'price',
-		],
-	],
-"processor" =>
-	[
-	"displayName" => "Processors",
-	"tableName" => "PROCESSOR",
-	"attributes" =>
-		[
-		'processor_id',
-		'name',
-		'qty',
-		'price',
-		],
-	],
-"processorStorage" =>
-	[
-	"displayName" => "Processor-Storage",
-	"tableName" => "PROCESSOR_STORAGE",
-	"attributes" =>
-		[
-		'processor_id',
-		'storage_id',
-		],
-	],
-"processorOs" =>
-	[
-	"displayName" => "Processor-OS",
-	"tableName" => "PROCESSOR_O_SYSTEM",
-	"attributes" =>
-		[
-		'processor_id',
-		'os_name',
-		],
-	],
-"processorMemory" =>
-	[
-	"displayName" => "Processor-Memory",
-	"tableName" => "PROCESSOR_MEMORY",
-	"attributes" =>
-		[
-		'processor_id',
-		'memory_id',
-		],
-	],
-];
+include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/tools/DatabaseMap.php';
 echo <<<EOD
 <div>
 	<form action = "/671Project/admin/views/AdminRead.php" method = "post">
@@ -174,20 +29,19 @@ echo <<<EOD
 </div>
 EOD;
 if (isset($_POST['tableInput'])) {
-	$tableName = $tables[$_POST['tableSelection']]["tableName"];
-	
 	echo <<<EOD
+	<h1>{$tables[$_POST['tableSelection']]['displayName']}</h1>
 	<div class = "divTable">
 		<div class = "divHeader">
 	EOD;
 	$attributes = $tables[$_POST['tableSelection']]["attributes"];
-	foreach ($attributes as $attribute) {
-		echo "<div class = 'divCell'>$attribute</div>";
+	foreach ($attributes as $attribute => $display) {
+		echo "<div class = 'divCell'>$display</div>";
 	}
 	echo "</div>";
-	foreach (regularQuery("SELECT * FROM {$tableName};") as $row) {
+	foreach (regularQuery("SELECT * FROM {$tables[$_POST['tableSelection']]['tableName']};") as $row) {
 		echo "<div class = 'divRow'>";
-		foreach ($attributes as $attribute) {
+		foreach ($attributes as $attribute => $display) {
 			echo "<div class = 'divCell'>{$row[$attribute]}</div>";
 		}
 		echo "</div>";
