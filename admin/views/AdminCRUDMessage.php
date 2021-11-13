d8640d9a12c3421265778e7eda3dea93e257252c
ccbrantley <c_brantl@uncg.edu>
2021-11-13 13:43:18 -0500

    Adding message for create operation.

--- admin/views/AdminCRUDMessage.php
+++ admin/views/AdminCRUDMessage.php
@@ -10,6 +10,15 @@ if (isset($_SESSION['delete_query_result'])) {
 	}
 	unset($_SESSION['delete_query_result']);
 }
+if (isset($_SESSION['create_query_result'])) {
+	if ($_SESSION['create_query_result']) {
+		$message = "<p class = 'successText'>Entry was successfully created.</p>";
+	}
+	else {
+		$message = "<p class = 'failedText'>Entry was not created.</p>";
+	}
+	unset($_SESSION['create_query_result']);
+}
 if ($message) { echo $message; }
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminFooter.php';
 ?>
