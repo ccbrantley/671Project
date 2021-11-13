b7c5aeaad557106605dbf2e28f4afeeec0cdc488
ccbrantley <c_brantl@uncg.edu>
2021-11-13 13:09:02 -0500

    Page to be used in redirect after every CRUD operation to display result messages.

---
+++ admin/views/AdminCRUDMessage.php
@@ -0,0 +1,15 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminHeader.php';
+$message = NULL;
+if (isset($_SESSION['delete_query_result'])) {
+	if ($_SESSION['delete_query_result']) {
+		$message = "<p class = 'successText'>Entry was successfully deleted.</p>";
+	}
+	else {
+		$message = "<p class = 'failedText'>Entry was not deleted.</p>";
+	}
+	unset($_SESSION['delete_query_result']);
+}
+if ($message) { echo $message; }
+include $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminFooter.php';
+?>
