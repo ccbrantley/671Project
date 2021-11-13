90446af3f700642c26dd239547837cc6cfe29e37
ccbrantley <c_brantl@uncg.edu>
2021-11-13 13:04:51 -0500

    Removes session, redirects to login.

--- admin/views/AdminLogout.php
+++ admin/views/AdminLogout.php
@@ -3,8 +3,5 @@ session_start();
 session_unset();
 session_destroy();
 include_once $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminHeader.php';
-echo <<<EOD
-	<div class = "logoutDiv"><p class = 'successText'>You have been successfully logged out.</p></div>
-EOD;
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminFooter.php';
 ?>
