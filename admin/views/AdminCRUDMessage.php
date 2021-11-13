9417522ed3f8b9e1cb8af4663080511dc458248e
ccbrantley <c_brantl@uncg.edu>
2021-11-13 14:49:33 -0500

    Adding message for update operation.

--- admin/views/AdminCRUDMessage.php
+++ admin/views/AdminCRUDMessage.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['delete_query_result'])) {
 	}
 	unset($_SESSION['delete_query_result']);
 }
-if (isset($_SESSION['create_query_result'])) {
+else if (isset($_SESSION['create_query_result'])) {
 	if ($_SESSION['create_query_result']) {
 		$message = "<p class = 'successText'>Entry was successfully created.</p>";
 	}
@@ -18,6 +18,15 @@ if (isset($_SESSION['create_query_result'])) {
 		$message = "<p class = 'failedText'>Entry was not created.</p>";
 	}
 	unset($_SESSION['create_query_result']);
+}
+else if (isset($_SESSION['update_query_result'])) {
+	if ($_SESSION['update_query_result']) {
+		$message = "<p class = 'successText'>Entry was successfully updated.</p>";
+	}
+	else {
+		$message = "<p class = 'failedText'>Entry was not updated.</p>";
+	}
+	unset($_SESSION['update_query_result']);
 }
 if ($message) { echo $message; }
 include $_SERVER['DOCUMENT_ROOT'] . '/671Project/admin/templates/AdminFooter.php';
