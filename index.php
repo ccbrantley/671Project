979232f54f747aa296c823228f67ada64b8402fb
ccbrantley <c_brantl@uncg.edu>
2021-11-14 20:32:40 -0500

    Adding name of store to homepage.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 	include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/header.php';
+	echo "<h1>671Computers</h1>";
 	include $_SERVER['DOCUMENT_ROOT'] . '/671Project/templates/footer.php';
 ?>
